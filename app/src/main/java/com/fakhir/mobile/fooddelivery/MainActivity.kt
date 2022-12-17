23113818e200f841b27ece89b81144961d12c511
fakhirsh <fakhir.shaheen@gmail.com>
2022-12-17 18:28:12 +0500

    Basic login screen frontend

--- app/src/main/java/com/fakhir/mobile/fooddelivery/MainActivity.kt
+++ app/src/main/java/com/fakhir/mobile/fooddelivery/MainActivity.kt
@@ -3,13 +3,16 @@ package com.fakhir.mobile.fooddelivery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.fakhir.mobile.fooddelivery.ui.theme.FoodDeliveryComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    LoginScreen()
                 }
             }
         }
@@ -39,5 +42,62 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     FoodDeliveryComposeTheme {
         Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreen() {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    FoodDeliveryComposeTheme{
+        Column(
+            modifier = Modifier.fillMaxSize(),
+
+        ) {
+            Image(
+                painter = painterResource(R.drawable.chinese_food_banner),
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth().height(150.dp),
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.panda),
+                    contentDescription = null,
+                    modifier = Modifier.wrapContentSize(align = Alignment.Center).height(150.dp)
+                )
+                Spacer(modifier = Modifier.height(35.dp))
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = {
+                        email = it
+                    },
+                    label = { Text(text = "Email") },
+                )
+
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = {
+                        password = it
+                    },
+                    label = { Text("Password") },
+                )
+
+                Button(
+                    onClick = { /* handle login */ },
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Text(text = "Log in")
+                }
+            }
+        }
     }
 }
